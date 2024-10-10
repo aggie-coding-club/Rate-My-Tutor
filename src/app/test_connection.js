cff0ed03ef63f4e9b0801f7b9a93545a05f2f435
Hurdamert <lichengtx@gmail.com>
2024-10-10 15:39:23 -0500

    mongodb connection demo

---
+++ src/app/test_connection.js
@@ -0,0 +1,30 @@
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+    /**
+     * Connection URI
+     */
+    const uri = "mongodb+srv://lichengtx:iloveratemytutor@users.y0ul8.mongodb.net/?retryWrites=true&w=majority&appName=users";
+    const { MongoClient } = require('mongodb');
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);

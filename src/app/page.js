5201dbdf45f23fd4de078c5453dd6bb2e38997c4
Kevin Chen <kchen2561@gmail.com>
2024-11-01 14:25:05 -0500

    bruh

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+'use client'
 import Image from "next/image";
-import { text } from "stream/consumers";
 import styles from './css/home.module.css';
 import RMT from './assets/RMT.png'
+import Link from 'next/link'
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 const background = {
   background: "white",
@@ -12,6 +16,26 @@ const background = {
 
 
 export default function Home() {
+  const [searchText, setSearchText] = useState('')
+  const router = useRouter();
+
+  const handleEnter = async (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      try {
+        await axios.post('/api/search', { query: searchTerm });
+        router.push(`/search?query=${encodeURIComponent(searchTerm)}`);
+      } catch (error) {
+        console.error('Error posting search term:', error);
+      }
+    }
+    }
+  const handleChange  = (e) => {
+    setSearchText(e.target.value)
+  }
+
+
+
   return (
     <div style={background}>
       {/* Landing page for once the user loads into the website */}
@@ -40,8 +64,16 @@ export default function Home() {
           {/* search section */}
           {/* TODO: add the search bar */}
           <div>
-            <h1>Enter your Tutor's name to get started.</h1>
-            <h1>Can't find your tutor? Click here</h1>
+            <div className={styles.searchSection}>
+              <h1 className={styles.helpComments} >Enter your Tutor's name to get started.</h1>
+              <form>
+                <input className={styles.searchbar} name="searchbar" type="text" placeholder="Search" onChange={handleChange}></input>
+              </form>
+              <div style={{display: "flex", flexDirection: "row"}}>
+                <h1 style={{paddingRight: "5px"}}>Can't find your tutor?</h1>
+                <Link href="/search" style={{textDecoration: "underline", color: "black"}}>Click here</Link>
+              </div>
+            </div>
           </div>
         </div>
         
